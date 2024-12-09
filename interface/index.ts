59cbff7b9c44c60dcf3bb1adf0b5c9053eba78c0
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-12-09 13:28:23 +0100

    update

---
+++ interface/index.ts
@@ -0,0 +1,7 @@
+export interface ButtonProps {
+  id: string; // Specify the type for id
+  title: string; // Specify the type for title
+  rightIcon?: React.ReactNode; // Specify the type for rightIcon
+  leftIcon?: React.ReactNode; // Specify the type for leftIcon
+  containerClass?: string; // Specify the type for containerClass
+}
